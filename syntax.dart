12529652c129ff734ff228276fdb0e25fc3239e0
Alex David <13235765+myitdev@users.noreply.github.com>
2018-07-31 18:45:09 +0800

    Create syntax.dart

---
+++ syntax.dart
@@ -0,0 +1,20 @@
+class printOut{
+  
+  void outPut(){
+    
+    print("hello world");
+  }
+  
+ 
+}
+
+void main(){
+    
+    printOut c = new printOut();
+    
+    c.outPut();
+  }
+
+/*
+The above example defines a class printOut. The class has a method outPut(). The method prints the string “Hello World” on the terminal. The new keyword creates an object of the class. The object invokes the method outPut()
+*/
