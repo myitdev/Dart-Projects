c4a6ce152ca8ff8fa0a6518ca606eb8f3ccc375b
Alex David <13235765+myitdev@users.noreply.github.com>
2018-07-31 20:53:23 +0800

    Create loop.dart

---
+++ loop.dart
@@ -0,0 +1,20 @@
+void main(){
+ // var i = 0;
+ var num = 5;
+  var factorize = 1;
+  
+  /*for( i= num; i>=1; i--){
+  
+  factorize *= i;
+  }
+  print(factorize);
+*/
+  // while loop
+  while( num >= 1){
+    
+    factorize *= num;
+    num--;
+   
+  }
+ print(factorize);
+}
